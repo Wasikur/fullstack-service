3662cec0217a7e0847ea05df0994e97c17093043
Wasikur <69371416+Wasikur@users.noreply.github.com>
2024-02-22 14:48:09 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:5000/api/form/contact`, {
+      const response = await fetch(`${API}/api/form/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(contact),
