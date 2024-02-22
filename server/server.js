3139ac57af8079db2739626f29928ee914a4c288
Wasikur <69371416+Wasikur@users.noreply.github.com>
2024-02-22 12:29:37 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./utils/db.js");
+
+const app = express();
+
+//Front-end routes
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router.js");
+const serviceRoute = require("./router/service-router.js");
+
+//Admin route
+const adminRoute = require("./router/admin-router.js");
+
+const errorMiddleware = require("./middlewares/error-middleware.js");
+
+// Handling cors
+const corsOptions = {
+  origin: "http://localhost:5173",
+  method: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Authentication api route
+app.use("/api/auth", authRoute);
+
+// Contact and Services api routes
+app.use("/api/form", contactRoute);
+app.use("/api/data", serviceRoute);
+
+// Admin api route
+app.use("/api/admin", adminRoute);
+
+// Using error detection middleware for exact problem
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT;
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+  });
+});
