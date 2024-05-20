f20ead8fbe20c76ef1f5031bdd5aea9c0e73ba81
Wasikur <69371416+Wasikur@users.noreply.github.com>
2024-05-20 11:08:01 +0530

    Update Analytics.jsx
    Added animation to the values of analytics counter

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -1,23 +1,34 @@
 import React from "react";
+import CountUp from "react-countup";
 import "./Analytics.css";
 const Analytics = () => {
+ const scrollDuration = 2;
   return (
     <section className="section-analytics">
       <div className="container grid grid-four-cols">
         <div className="div1">
-          <h2>50+</h2>
-          <p>Registered Companies</p>
+          <h2>
+            <CountUp end={800} duration={scrollDuration} />+
+          </h2>
+          <p>Registered Properties</p>
         </div>
         <div className="div1">
-          <h2>1000+</h2>
-          <p>Happy Clients</p>
+          <h2>
+            <CountUp end={1000} duration={scrollDuration} />+
+          </h2>
+          <p>Satisfied Customers</p>
         </div>
         <div className="div1">
-          <h2>500+</h2>
-          <p>Well Know Developers</p>
+          <h2>
+            <CountUp end={500} duration={scrollDuration} />+
+          </h2>
+          <p>Happy Property Owners</p>
         </div>
         <div className="div1">
-          <h2>24/7</h2>
+          <h2>
+            <CountUp end={24} duration={scrollDuration} />
+            /7
+          </h2>
           <p>Service</p>
         </div>
       </div>
